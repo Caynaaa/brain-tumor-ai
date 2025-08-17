3fb1020adfdf78f8d7ea48bb27da8e5d77a05833
Cayanaaa <mazzam0504@gmail.com>
2025-08-18 01:06:10 +0700

    fixed-typo again -_-

--- Models/2D_Classifier_Binary/utils/__init__.py
+++ Models/2D_Classifier_Binary/utils/__init__.py
@@ -1,3 +1,3 @@
 from .seed import set_seed
 from .hf_to_tuple import hf_dataset_to_tuple
-from .imbalanced_data_utils import get_poss_weight, make_weights_sampler
+from .imbalanced_data_utils import compute_poss_weight, make_weights_sampler
