a57c79fbed26a48bdb9fd24fa07912c2caa7d762
Cayanaaa <mazzam0504@gmail.com>
2025-08-19 22:21:28 +0700

    update and add func in utils (random_sampler, class_weight, pos_weight)

---
+++ Models/2D_Classifier_Binary/utils/class_weight_loss.py
@@ -0,0 +1,22 @@
+from sklearn.utils.class_weight import compute_class_weight
+import torch
+import numpy as np
+
+def class_weights(labels: torch.tensor, num_classes: int) -> torch.tensor:
+    """
+    Compute class weights for imbalanced datasets.
+
+    Args:
+        labels (torch.Tensor): Tensor of shape (N,) containing class labels.
+        num_classes (int): Total number of classes.
+    
+    Returns:
+        torch.Tensor: Weights for each class (size = num_classes).
+    """
+    labels = np.array(labels.cpu())
+    class_weights = compute_class_weight(
+        class_weight = "balanced",
+        classes = np.arange(num_classes),
+        y = labels
+    )
+    return torch.tensor(class_weights, dtype=torch.float)
---
+++ Models/2D_Classifier_Binary/utils/random_sampler.py
@@ -0,0 +1,28 @@
+import torch
+
+def make_random_sampler(labels: torch.tensor) -> torch.utils.data.WeightedRandomSampler:
+    """
+    Create a WeightedRandomSampler to handle imbalanced datasets.
+
+    This sampler assigns higher weights to underrepresented classes so that
+    the DataLoader samples them more frequently during training.
+
+    Args:
+        labels (torch.Tensor): 1D tensor of shape (N,) containing binary labels (0 or 1).
+
+    Returns:
+        torch.utils.data.WeightedRandomSampler: Sampler object for DataLoader.
+    """
+    
+    # Count samples for each class
+    class_sample_count = torch.tensor([(labels == t).sum() for t in torch.unique(labels)])
+    # Invert counts to weights(less frequent classes get higher weights)
+    weight = 1. / class_sample_count.float()
+    # Assign weights to each sample based on its class
+    samples_weight = torch.tensor([weight[t] for t in labels])
+    
+    return torch.utils.data.WeightedRandomSampler(
+        weights=samples_weight,
+        num_samples=len(samples_weight),
+        replacement=True  # Sample with replacement
+    )
